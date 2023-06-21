13631102d701622c7136c5ca5582b0370d0c9e6a
akarshabhardwaj <akarshabhardwaj7280976503@gmail.com>
2023-06-21 19:01:33 +0530

    added readme

--- itxaeasy/src/App.js
+++ itxaeasy/src/App.js
@@ -2,7 +2,6 @@ import logo from './logo.svg';
 import './App.css';
 import AllRoutes from './Routes/AllRoutes';
 import Navbar from './Components/Navbar';
-import HRAForm from './Components/GptForm';
 
 function App() {
   return (
